377940b116a15793de811ed275d3fd1b9e827d8f
pedroaugustobborges <97298891+pedroaugustobborges@users.noreply.github.com>
2023-11-06 02:49:57 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -245,6 +245,37 @@ col3.plotly_chart(fig_donut)
 
 #ULTIMO GRAFICO
 
+# Calculate the Quantity of "ATRIBUÍDO" (total number of rows in filtered_df)
+quantity_atribuido = len(filtered_df)
+
+# Calculate the Quantity of "FINALIZADO"
+if quarter == "Todos" and month == "Todos":
+    # When both "Trimestre" and "Mês" are "Todos", quantity_finalizado is the count of values in "FORMULA 1" in df
+    quantity_finalizado = quantity_residual - (df1["FORMULA 1"] == 2).sum()
+else:
+    # When at least one of "Trimestre" or "Mês" is not "Todos"
+    if quarter == "Todos":
+        # When only "Trimestre" is "Todos", use the first value in "Year-Month conclusion" of filtered_df
+        first_year_month = filtered_df["Year-Month conclusion"].iloc[0]
+        quantity_finalizado = (df["Year-Month conclusion"] == first_year_month).sum()
+    else:
+        if month == "Todos":
+            # When only "Mês" is "Todos", use the first value in "Year-Quarter conclusion" of filtered_df
+            first_year_quarter = filtered_df["Year-Quarter conclusion"].iloc[0]
+            quantity_finalizado = (df["Year-Quarter conclusion"] == first_year_quarter).sum()
+        else:
+            # When both "Trimestre" and "Mês" are not "Todos", quantity_finalizado is 0
+            quantity_finalizado = 0
+
+# Calculate the Quantity of "RESIDUAL"
+quantity_residual = quantity_atribuido - quantity_finalizado
+
+# Add the "quantity_residual" column to the DataFrame
+filtered_df["quantity_residual"] = quantity_residual
+
+# Create a line chart to visualize the evolution of quantity_residual over time
+fig_residual_evolution = px.line(filtered_df, x="Year-Month conclusion", y="quantity_residual", title="Evolution of Residual Quantity Over Time")
+st.plotly_chart(fig_residual_evolution)
 
 
 
