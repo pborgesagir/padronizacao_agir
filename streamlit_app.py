4ec225485673262d21b68bb0118553f4da148445
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2024-03-06 15:50:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,12 @@ import plotly.graph_objects as go
 import numpy as np
 
 
-st.set_page_config(layout="wide",
-                  page_icon="https://media.licdn.com/dms/image/C4D0BAQHXylmAyGyD3A/company-logo_200_200/0/1630570245289?e=2147483647&v=beta&t=Dxas2us5gteu0P_9mdkQBwJEyg2aoc215Vrk2phu7Bs")
+st.set_page_config(
+    page_title='Dashboard - Padronização AGIR',
+    layout='wide',
+    page_icon="https://media.licdn.com/dms/image/C4D0BAQHXylmAyGyD3A/company-logo_200_200/0/1630570245289?e=2147483647&v=beta&t=Dxas2us5gteu0P_9mdkQBwJEyg2aoc215Vrk2phu7Bs",
+    initial_sidebar_state='auto'
+)
 url = "https://docs.google.com/spreadsheets/d/1CGypJWn35SFD6oYeLmFU1HjMhxJRBpuuVuVeP7y86wo/edit#gid=0"
 st.title("DASHBOARD - PADRONIZAÇÃO AGIR")
 conn = st.connection("gsheets", type=GSheetsConnection)
